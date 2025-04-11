1e7905f30e80c28029a77c43438dd28286902837
Lani Villamor <lanivillamor@lanis-air.home>
2025-04-11 18:59:10 -0400

    Add handleAddTask in CreateItem Add handleCeckbox and item id and useEffect for henerating todolist

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,8 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CreateItem from "./CreateItem";
+import axios from "axios";
 
 function Page() {
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/get")
+      .then((result) => setTodos(result.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleCheckbox = (id) => {
+    axios
+      .put(`http://localhost:3001/update/${id}`)
+      .then((result) => console.log(result))
+      .catch((error) => console.log(error));
+  };
 
   return (
     <div className="page">
@@ -13,7 +28,19 @@ function Page() {
           <h3>No Record</h3>
         </div>
       ) : (
-        todos.map((todo) => <div>{todo}</div>)
+        todos.map((todo) => (
+          <div key={todo._id} className="page__task">
+            <input
+              type="checkbox"
+              onClick={() => {
+                handleCheckbox(todo._id);
+              }}
+            />
+            <span className="checkmark">
+              <p className="page__task-item">{todo.task}</p>
+            </span>
+          </div>
+        ))
       )}
     </div>
   );
